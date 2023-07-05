df4f7d31d4f910b4ebf6e9c737aa113e043223fb
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-05 16:34:01 -0400

    add functions closure example 2 complete

--- 05 closure complete example/app.js
+++ 05 closure complete example/app.js
@@ -1,17 +1,31 @@
-//closure bsic example
+//closure basic example
 
 function newAccount(name, initialBalance) {
   let balance = initialBalance;
   function showBalance() {
     console.log(`hey ${name} your balance is $${balance}`);
   }
-  return showBalance;
-}
+  function deposit(amount) {
+    balance += amount;
+    showBalance();
+  }
+  function withdraw(amount) {
+    if (amount > balance) {
+      console.log(`hey ${name}, sorry not enoug funds`);
+      return;
+    }
+    balance -= amount;
+    showBalance();
+  }
 
-newAccount('tom', 77)();
+  return { showBalance: showBalance, deposit: deposit, withdraw, withdraw };
+}
 
 const john = newAccount('john', 88);
 const will = newAccount('will', 99);
 
-john();
-will();
+john.showBalance();
+john.deposit(400);
+john.deposit(400);
+john.withdraw(889);
+will.showBalance();
