5d7569c3606db6cbb2e88f88e25f5df9185c913d
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-23 13:51:57 -0400

    add functions iiffe lesson

---
+++ iffe/app.js
@@ -0,0 +1,4 @@
+//iife immediatley invoked function expression
+//iiffe is an older approach - modules are used nowadays
+//simple approach to avoid global scope pollution
+//i.e. good way at protecting the scope of your function and the vairables within it
