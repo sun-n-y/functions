d5b029f19f81b1c90e19775f367f36f2e47391f3
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-23 14:08:45 -0400

    add functions iiffe lesson complete

--- iffe/app.js
+++ iffe/app.js
@@ -2,3 +2,33 @@
 //iiffe is an older approach - modules are used nowadays
 //simple approach to avoid global scope pollution
 //i.e. good way at protecting the scope of your function and the vairables within it
+
+//normal setup,
+const num1 = 30;
+const num2 = 50;
+
+function add() {
+  console.log(`the result is : ${num1 + num2}`);
+}
+
+add();
+
+//iiffe setup,name not needed,variables inside cannot be accessed outisde
+(function () {
+  const num3 = 30;
+  const num4 = 50;
+  console.log(`the result is : ${num3 + num4}`);
+})();
+
+//pass in args
+(function (num3, num4) {
+  console.log(`the result is : ${num3 + num4}`);
+})(500, 500);
+
+//return and assign to variable
+const result = (function (num3, num4) {
+  console.log(`the result is : ${num3 + num4}`);
+  return num3 + num4;
+})(500, 500);
+
+console.log(result);
